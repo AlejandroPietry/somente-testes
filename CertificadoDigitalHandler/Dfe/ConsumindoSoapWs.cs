55a52a991ac5a62277220392854650f4d21dbdc1
Alejandro Pietry <Alejandro_trindade@econeteditora.com.br>
2022-02-07 10:29:47 -0300

    soap funfando

--- CertificadoDigitalHandler/Dfe/ConsumindoSoapWs.cs
+++ CertificadoDigitalHandler/Dfe/ConsumindoSoapWs.cs
@@ -1,6 +1,7 @@
 ﻿using CertificadoDigitalHandler.Dfe.Common;
 using System;
 using System.IO;
+using System.IO.Compression;
 using System.Net;
 using System.Security.Cryptography.X509Certificates;
 using System.Text;
@@ -14,7 +15,7 @@ namespace CertificadoDigitalHandler.Dfe
         private const string WsUrl = @"https://mdfe.svrs.rs.gov.br/ws/MDFeDistribuicaoDFe/MDFeDistribuicaoDFe.asmx";
         private const string pathCertificado = @"C:\inetpub\wwwroot\ArquivosNFeUpload\43870_21072669000320_cert2022.pfx";
         private const string senhaCertificado = "UrsinhoOuro@2022";
-        private const string UrlFazenda = @"https://mdfe.svrs.rs.gov.br/ws/MDFeDistribuicaoDFe/MDFeDistribuicaoDFe.asmx";
+        private const string UrlFazenda = @"https://www1.nfe.fazenda.gov.br/NFeDistribuicaoDFe/NFeDistribuicaoDFe.asmx";
 
         public void Start()
         {
@@ -53,21 +54,16 @@ namespace CertificadoDigitalHandler.Dfe
             string NSU, base64, xmlNota;
 
             //Criação do xml
-            string xml = "<?xml version=\"1.0\" encoding=\"utf-8\"?>" +
-                        "<distDFeInt versao=\"1.00\" xmlns=\"http://www.portalfiscal.inf.br/nfe\">" +
-                        "<tpAmb>1</tpAmb>" +
-                        "<cUFAutor>35</cUFAutor>" +
-                        "<CNPJ>21072669000320</CNPJ>" +
-                        "<distNSU>" +
-                        "<ultNSU>000000000000000</ultNSU>" +
-                        "</distNSU>" +
-                        "</distDFeInt>";
+            string xml = File.ReadAllText(@"C:\Users\DEV\Documents\XmlSoap.txt");
 
             //Convertendo a string em xml
             doc.LoadXml(xml);
 
             string resultado = SendRequestAsync(doc, certificadoX509);
 
+
+            XmlDocument responseXmlDocument = new XmlDocument();
+            responseXmlDocument.LoadXml(resultado);
         }
 
         public string SendRequestAsync(XmlDocument xmlEnvelop, X509Certificate2 certificadoDigital)
@@ -85,12 +81,35 @@ namespace CertificadoDigitalHandler.Dfe
             streamWriter.Write(xmlSoap);
             streamWriter.Close();
 
-            using(HttpWebResponse httpResponse = (HttpWebResponse)httpWr.GetResponse())
-            using(var streamReader = new StreamReader(httpResponse.GetResponseStream()))
+            using HttpWebResponse httpResponse = (HttpWebResponse)httpWr.GetResponse();
+            using var streamReader = new StreamReader(httpResponse.GetResponseStream());
+            string xmlRetorno = streamReader.ReadToEnd();
+            return xmlRetorno;
+        }
+
+        public string Decompress(byte[] gzip)
+        {
+            using (GZipStream stream = new GZipStream(new 
+                MemoryStream(gzip), CompressionMode.Decompress))
             {
-                string xmlRetorno = streamReader.ReadToEnd();
-                return xmlRetorno;
+                const int size = 4096;
+                byte[] buffer = new byte[size];
+                using (MemoryStream memory = new MemoryStream())
+                {
+                    int count = 0;
+                    do
+                    {
+                        count = stream.Read(buffer, 0, size);
+                        if (count > 0)
+                        {
+                            memory.Write(buffer, 0, count);
+                        }
+                    }
+                    while (count > 0);
+                    return Encoding.UTF8.GetString(memory.ToArray());
+
+                }
             }
-        }    
+        }
     }
 }
